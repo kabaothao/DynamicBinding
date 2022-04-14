5c2bf65f3b4ea77cb215720c5650280290fc09d0
Kabao Thao <KabaoKThao@gillettechildrens.com>
2022-04-14 09:17:12 -0500

    Add project files.

---
+++ DynamicBinding/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DynamicBinding // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            object obj = "Mosh";
+            //obj.GethashCode()
+
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo.Invoke(null, null);
+        }
+    }
+}
